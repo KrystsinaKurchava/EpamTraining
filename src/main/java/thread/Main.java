b8315d809d742c3147703def6cfbd180bdffbb81
Krystsina Kurchava <krystsina.kurchava@gmail.com>
2019-10-13 19:24:15 +0300

    fixed packages name

--- src/main/java/thread/Main.java
+++ src/main/java/thread/Main.java
@@ -1,4 +1,4 @@
-package threads;
+package thread;
 
 public class Main {
     public static void main(String args[]) throws InterruptedException {
--- src/main/java/thread/ParkingPlace.java
+++ src/main/java/thread/ParkingPlace.java
@@ -1,4 +1,4 @@
-package threads;
+package thread;
 
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
